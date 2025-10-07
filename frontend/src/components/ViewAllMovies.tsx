72cb1f7b04591fd3e1fad8b19a9574e1bc090533
Adrian Kleinlosen <adriankleinlosen@gmail.com>
2025-10-07 14:16:17 +0200

    wrote unit test for getAllMovies method, corrected a few typos, and deleted "Hello World" from App.tsx so not to display it

--- frontend/src/components/ViewAllMovies.tsx
+++ frontend/src/components/ViewAllMovies.tsx
@@ -8,7 +8,7 @@ export default function ViewAllMovies() {
 
     useEffect(() => {
         axios.get("/api/movies")
-            .then(resonse => setMovies(resonse.data))
+            .then(response => setMovies(response.data))
             .catch(error => console.error("Fehler beim Laden der Filme", error));
     }, []);
 
@@ -18,14 +18,14 @@ export default function ViewAllMovies() {
             {movies.length === 0 ? (
                 <p>Keine Filme gefunden</p>
             ) : (
-                <p>
+                <ul>
                     {
                         movies.map(movie =>
                             <li key={movie.id}>
                                 <strong>{movie.title}</strong> – {movie.author}
                             </li>)
                     }
-                </p>
+                </ul>
             )}
         </>
 
