b9808911b0aff1cd265be2995971aeaf8c1deccf
Dennis Lemmer <dlemmer28@gmail.com>
2025-10-09 17:10:38 +0200

    simplified the frontpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,10 @@
-import './App.css'
-import axios from "axios";
-import {useEffect, useState} from "react";
-import MovieSearch from "./components/MovieSearch.tsx";
+import './App.css';
+import MovieSearch from "./components/MovieSearch";
 
 export default function App() {
-    const [message, setMessage] = useState<string>("")
-
-    useEffect(() => {
-        axios.get("/api/hello")
-            .then(response => {
-                console.log("Antwort vom Backend:", response.data.value);
-                setMessage(response.data);
-            })
-            .catch(error => {
-                console.error("Fehler beim Laden", error);
-            });
-    }, []);
-
     return (
         <div id="root">
-            <h1>{message || "Movie App"}</h1>
+            <h1 style={{ textAlign: "center", marginTop: "2rem" }}>Movie App</h1>
             <MovieSearch />
         </div>
     );
