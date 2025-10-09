c22f6f7f277a9945949aedd5b5c49de914ea7db1
Dennis Lemmer <dlemmer28@gmail.com>
2025-10-09 16:57:07 +0200

    added responseDto to deal with json responses

---
+++ backend/src/main/java/org/example/backend/model/OmdbSearchResponseDto.java
@@ -0,0 +1,25 @@
+package org.example.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record OmdbSearchResponseDto(
+        @JsonProperty("Search")
+        List<OmdbMovieShortDto> search
+) {
+
+    public List<Movie> toMovieList() {
+        if (search == null) return List.of();
+
+        return search.stream()
+                .map(s -> new Movie(
+                        null,               // id in MongoDB
+                        s.title(),
+                        s.year(),
+                        "", "", "", "", "", "", "", "", s.poster(), "", s.imdbID()
+                ))
+                .collect(Collectors.toList());
+    }
+}
