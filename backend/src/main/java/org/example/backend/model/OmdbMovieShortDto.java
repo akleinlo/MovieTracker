8a078adb85a5d9c6ad9396e0ea2586d53062ceb2
Dennis Lemmer <dlemmer28@gmail.com>
2025-10-09 16:57:29 +0200

    added shortResponseDto to deal with json responses

---
+++ backend/src/main/java/org/example/backend/model/OmdbMovieShortDto.java
@@ -0,0 +1,17 @@
+package org.example.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record OmdbMovieShortDto(
+        @JsonProperty("Title")
+        String title,
+        @JsonProperty("Year")
+        String year,
+        @JsonProperty("imdbID")
+        String imdbID,
+        @JsonProperty("Type")
+        String type,
+        @JsonProperty("Poster")
+        String poster
+) {
+}
