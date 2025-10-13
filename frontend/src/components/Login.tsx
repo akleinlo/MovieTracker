96e902580dab20f72084c7faa675a88293488025
Adrian Kleinlosen <adriankleinlosen@gmail.com>
2025-10-13 11:31:44 +0200

    prevented infinitive loop in Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,19 +3,22 @@ import {useEffect} from "react";
 
 export default function Login() {
     const host: string = window.location.host === "localhost:5173" ? "http://localhost:8080" : window.location.origin
-    window.open(host + "/oauth2/authorization/github", "_self")
+
+    const handleLogin = () => {
+        window.open(host + "/oauth2/authorization/github", "_self");
+    };
 
     const loadUser= () => {
         axios.get("/api/auth/me")
             .then(response => console.log(response.data))
     }
     useEffect(() => {
-        loadUser()
+        loadUser();
     }, []);
 
     return (
         <div>
-            <button onClick={Login}>Login</button>
+            <button onClick={handleLogin}>Login</button>
         </div>
     )
 }
