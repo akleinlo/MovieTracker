20895d8bdfbb12cdb8a38f6c4ccddd3ea5d9ebc1
Dennis Lemmer <dlemmer28@gmail.com>
2025-10-15 15:07:16 +0200

    changed OmdbApiService, removed @value and added configs to Backend, OmdbApiService and added test for AuthController

--- backend/src/test/java/org/example/backend/security/AuthControllerTest.java
+++ backend/src/test/java/org/example/backend/security/AuthControllerTest.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,7 +19,11 @@ class AuthControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void getMe() {
+    void getMe() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth/me")
+                        .with(oidcLogin().userInfoToken(token -> token.claim("login", "testUser"))))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("testUser"));
 
     }
 }
